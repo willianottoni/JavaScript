ccc6c5d3a7662f55fff13044d4363d87c7aaafcf
aluno <aluno@ftcbp.edu.br>
2022-02-25 11:33:13 -0300

    Aula 25 Fev

--- Calculadora/script.js
+++ Calculadora/script.js
@@ -1,8 +1,49 @@
+let flagApaga = false;
+let numAux = 0;
+
 function insertNumber(element) {
-    let display = document.getElementById("display-calculator");
-    display.value += element.value;
+    const display = document.getElementById("display-calculator")
+    if (display.value == 0 || flagApaga == true) {
+        display.value = element.value;
+    } else {
+        display.value += element.value;
+    }
+    flagApaga = false;
+}
+
+function insertOperacion(element) {
+    const displayResult = document.getElementById("display-result")
+    displayResult.value = element.value
+    flagApaga = true;
+    numAux = document.getElementById("display-calculator").value
+}
+
+function result(){
+    let result = 0;
+    let parcela = document.getElementById("display-calculator").value
+    operacion = document.getElementById("display-result").value
+    switch (operacion){
+        case "+":
+            result = numAux + parcela
+            break;
+        case "-":
+            result = numAux - parcela
+            break;
+        case "x":
+            result = numAux * parcela
+            break;
+        case "/":
+            result = numAux / parcela
+            break;
+    }
+    document.getElementById("display-calculator").value = result
+    numAux = 0
+    document.getElementById("display-result").value = ""
+    flagApaga = true
 }
 
 function clearDisplay() {
-    document.getElementById("display-calculator").innerHTML = " "
+    document.getElementById("display-calculator").value = ""
+    document.getElementById("display-result").value = ""
 }
+
